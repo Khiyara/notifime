9973f9e5491aa4490fc2c47a7be71718b6abb343
Khiyara <rizkhiphastiputra@gmail.com>
2020-12-24 21:30:51 +0700

    fix: color background on splash loadingscreen

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/rizkhiph/notifime/ui/splashscreen/view/OpenGLRender.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/rizkhiph/notifime/ui/splashscreen/view/OpenGLRender.java
@@ -69,7 +69,7 @@ public class OpenGLRender implements GLSurfaceView.Renderer {
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
         long currentTimeMillis = System.currentTimeMillis();
